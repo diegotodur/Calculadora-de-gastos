fab7cc76c671d3e8b913d112cfad8c79b81e573f
DiegoTorres <miscelanneo@gmail.com>
2023-04-29 09:43:44 -0400

    new styles and functions js

--- SRC/JS/index.js
+++ SRC/JS/index.js
@@ -1,8 +1,21 @@
-document.getElementById(descargardata).addEventListener('click', function(){
-  var table2excel = new Table2Excel();
-      table2excel.export(document.querySelectorAll("table"));
-   })
+//ACTUALIZAR VALOR DE INGRESOS
+
+const actualizarBtn = document.getElementById('botonactualizar');
+
+actualizarBtn.addEventListener('click', function() {
+  const ingresos = Number(document.getElementById('valoringresos').value);
+  
+  const presupuestoActual = document.getElementById('presupuesto-actual');
+  
+  presupuestoActual.textContent = `$${ingresos}`;
+  actualizarSaldo()
+});
+
+//FUNCION SALDO
+const saldo = document.getElementById('saldo-actual');
 
 function actualizarSaldo(){
-  var saldo = presupuesto - gastos
+  const ingresos = Number(document.getElementById('valoringresos').value);
+  saldo.textContent= `$${ingresos}`
+
 }
