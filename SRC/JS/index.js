bfca3584fdfbda4b0ff2d9fd266ace86042ab687
DiegoTorres <miscelanneo@gmail.com>
2023-04-28 23:35:29 -0400

    colors and nav

--- SRC/JS/index.js
+++ SRC/JS/index.js
@@ -2,3 +2,7 @@ document.getElementById(descargardata).addEventListener('click', function(){
   var table2excel = new Table2Excel();
       table2excel.export(document.querySelectorAll("table"));
    })
+
+function actualizarSaldo(){
+  var saldo = presupuesto - gastos
+}
